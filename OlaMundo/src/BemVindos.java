7305722e3dbf095fe0962a3c3288af14f18b3c27
kiadocicco <kiadocicco@yahoo.com.br>
2023-01-13 21:51:17 -0300

    versao1

---
+++ OlaMundo/src/BemVindos.java
@@ -0,0 +1,23 @@
+/*projeto1*/
+
+public class BemVindos {
+
+	public static void main(String[] args) {
+		System.out.println("Olá seja bem vindo o mundo JAVA");
+//para colocar comentários coloque duas barras
+		
+		// Declarando variável
+		String nome; // variavel do tipo texto
+		int idade; // variável do tipo inteiro
+		// Inserindo valores nas variáveis
+		nome="Kátia";
+		idade=48;
+		// mostrar nome e idade do usuário
+		System.out.println("Prezado(a):" + nome + "\nVocê tem: "+ idade + " anos");
+		// barra n /n faz quebra de linha
+		
+		
+		
+	}
+
+}
